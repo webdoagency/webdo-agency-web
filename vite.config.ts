6878b49c2c153c11170e3d8c7e56f45f1c1e887e
webdoagency <webdonow@gmail.com>
2025-08-18 15:10:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",   // 👈 Important for GitHub Pages + custom domain
   server: {
     host: "::",
     port: 8080,
